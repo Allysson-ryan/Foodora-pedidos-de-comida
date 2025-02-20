6aaee7d14d671a38f97e359c323e12db27202492
Allysson Ryan <allyssonr002@gmail.com>
2025-02-20 19:22:27 -0300

    updated

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,11 +1,105 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../assets/logo.svg';
+import footer from '../assets/footer.jpg';
+import { FOOTER_CONTACT_INFO, FOOTER_LINKS } from '../assets/data';
+import { MdSocialDistance } from 'react-icons/md';
+import {
+  FaFacebook,
+  FaInstagram,
+  FaLinkedin,
+  FaTwitter,
+  FaYoutube,
+} from 'react-icons/fa6';
 
 const Footer = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const SOCIALS = {
+    title: 'Rede Social',
+    links: [
+      <FaFacebook />,
+      <FaInstagram />,
+      <FaTwitter />,
+      <FaYoutube />,
+      <FaLinkedin />,
+    ],
+  };
 
-export default Footer
+  return (
+    <footer>
+      {/* main container */}
+      <div>
+        {/* footer columns container */}
+        <div>
+          <div>
+            <div>
+              {/* logo */}
+              <Link to={'/'} className="bold-24 flex-1 flex items-baseline">
+                <img
+                  src={logo}
+                  alt="logo-img"
+                  height={24}
+                  width={24}
+                  className="hidden sm:flex"
+                />
+                <span className="text-secondary pl-2"> Tando </span> ori
+              </Link>
+              <div>
+                <p>
+                  Servimos refeições feitas com os ingredientes mais frescos e
+                  finos diariamente.
+                </p>
+                <img src={footer} alt="" className="rounded-md mt-6 w-44" />
+              </div>
+            </div>
+            {FOOTER_LINKS.map((col) => (
+              <FooterColumn key={col.title} title={col.title}>
+                <ul className="flex flex-col gap-4 regular-14 text-gray-20">
+                  {col.links.map((link, i) => (
+                    <Link to={'/'} key={i}>
+                      {link}
+                    </Link>
+                  ))}
+                </ul>
+              </FooterColumn>
+            ))}
+            <div>
+              <FooterColumn title={FOOTER_CONTACT_INFO.title}>
+                {FOOTER_CONTACT_INFO.links.map((link, i) => (
+                  <Link
+                    to={'/'}
+                    key={i}
+                    className="flex gap-4 md:flex-col lg:flex-row"
+                  >
+                    <p>{link.label}:</p> <p className="bol-15">{link.value}</p>
+                  </Link>
+                ))}
+              </FooterColumn>
+            </div>
+            <div className="flex">
+              <FooterColumn title={SOCIALS.title}>
+                <ul className="flex gap-4">
+                  {SOCIALS.links.map((link, i) => (
+                    <Link to={'/'} className="text-xl">
+                      {link}
+                    </Link>
+                  ))}
+                </ul>
+              </FooterColumn>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+const FooterColumn = ({ title, children }) => {
+  return (
+    <div className="flex flex-col gap-5">
+      <h4 className="bold-18 whitespace-nowrap">{title}</h4>
+      {children}
+    </div>
+  );
+};
+
+export default Footer;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,6 +3,9 @@ import Hero from './../components/Hero';
 import Features from './../components/Features';
 import Process from '../components/Process';
 import PopularFoods from './../components/PopularFoods';
+import coverBanner1 from '../assets/cover-banner-1.jpg';
+import coverBanner2 from '../assets/cover-banner-2.jpg';
+import Footer from '../components/Footer';
 
 const Home = () => {
   return (
@@ -11,6 +14,23 @@ const Home = () => {
       <Features />
       <Process />
       <PopularFoods />
+      <div className="max-w-[1440] mx-auto flex flex-col md:flex-row gap-7 py-12">
+        <div>
+          <img
+            src={coverBanner1}
+            alt="coverBanner1"
+            className="md:rounded-e-2xl"
+          />
+        </div>
+        <div>
+          <img
+            src={coverBanner2}
+            alt="coverBanner2"
+            className="md:rounded-s-2xl"
+          />
+        </div>
+      </div>
+      <Footer />
     </>
   );
 };
